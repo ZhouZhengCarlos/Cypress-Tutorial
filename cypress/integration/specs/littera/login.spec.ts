522d158e7d6bba145fa44c083c527f82317bc49a
zhouzhengcarlos <carloszhou0@gmail.com>
2022-03-25 12:05:43 -0600

    style: Changing arrow function to function, for a better use of fixtures

--- cypress/integration/specs/littera/login.spec.ts
+++ cypress/integration/specs/littera/login.spec.ts
@@ -2,17 +2,16 @@ import LoginPage from '@pageobjects/login/login.page'
 import StudentPage from '@pageobjects/student/student.page'
 
 describe('Login Littera', () => {
-    let student: { email: string; password: string; }
-    beforeEach(() => {
+    beforeEach(function(){
         cy.visit(LoginPage.URL)
         cy.fixture('credentials.json').then((user) => {
-             student = user.student.email
+             this.student = user.student.email
         })
     })
     
-    it('Login as student', () => {
-        cy.get(LoginPage.inputEmail).type(student.email)
-        cy.get(LoginPage.inputPassword).type(student.password)
+    it('Login as student', function(){
+        cy.get(LoginPage.inputEmail).type(this.student.email)
+        cy.get(LoginPage.inputPassword).type(this.student.password)
         cy.get(LoginPage.btnSignIn).click()
         StudentPage.verifyUrl()
         cy.contains("Hello Alvaro")
